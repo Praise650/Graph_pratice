8be90367f141e7873ffb44f38abf44a9fef0e60e
hp <ayokanmi1275@gmail.com>
2023-01-09 13:26:27 +0100

    Added graph screen

---
+++ lib/ui/screens/graph_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GraphScrenn extends StatefulWidget {
+  const GraphScrenn({Key? key}) : super(key: key);
+
+  @override
+  State<GraphScrenn> createState() => _GraphScrennState();
+}
+
+class _GraphScrennState extends State<GraphScrenn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
